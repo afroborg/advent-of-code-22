85a6cd2887a2590ea8f29cee32aff9db8f3b6818
Axel Froborg <axel.froborg@gmail.com>
2022-12-09 16:35:54 +0100

    day 3 part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,21 +1,38 @@
-use std::{fs, str::FromStr};
+use std::{collections::HashSet, fs};
 
+#[derive(Debug, Clone)]
 struct Rucksack {
-    compartment_one: Vec<char>,
-    compartment_two: Vec<char>,
+    all: Vec<char>,
+    compartments: (Vec<char>, Vec<char>),
 }
 
 impl Rucksack {
-    fn shared_items(&self) -> Vec<&char> {
-        let mut shared_items = Vec::new();
+    fn new(s: &str) -> Self {
+        let items = s.chars().collect::<Vec<_>>();
+        let size = items.len() / 2;
+        let compartment_one = items[0..size].to_vec();
+        let compartment_two = items[size..].to_vec();
 
-        for item in self.compartment_one.iter() {
-            if self.compartment_two.contains(&item) {
-                shared_items.push(item);
-            }
+        Rucksack {
+            all: items,
+            compartments: (compartment_one, compartment_two),
         }
+    }
 
-        shared_items
+    fn shared_items(&self) -> Vec<&char> {
+        let (compartment_one, compartment_two) = &self.compartments;
+
+        let compartment_one: HashSet<_> = compartment_one.iter().collect();
+        let compartment_two: HashSet<_> = compartment_two.iter().collect();
+
+        compartment_one
+            .intersection(&compartment_two)
+            .copied()
+            .collect()
+    }
+
+    fn to_priority_values(&self) -> Vec<i32> {
+        self.all.iter().map(|c| map_to_value(c)).collect()
     }
 }
 
@@ -25,36 +42,91 @@ fn main() {
     println!("Part 2: {}", solve_part_2(&data));
 }
 
+fn get_rucksacks(data: &str) -> Vec<Rucksack> {
+    data.lines().map(Rucksack::new).collect()
+}
+
+fn map_to_value(c: &char) -> i32 {
+    match c {
+        'a'..='z' => *c as i32 - 96,
+        'A'..='Z' => *c as i32 - 38,
+        _ => 0,
+    }
+}
+
+fn to_priority_values(rucksack: &Rucksack) -> Vec<i32> {
+    rucksack
+        .shared_items()
+        .iter()
+        .map(|c| map_to_value(*c))
+        .collect()
+}
+
+fn get_prioritized_value(rucksack: &Rucksack) -> i32 {
+    to_priority_values(rucksack)
+        .iter()
+        .max()
+        .unwrap()
+        .to_owned()
+}
+
 fn solve_part_1(data: &str) -> String {
-    let rucksacks = data.lines().map(|rucksack| {
-        let items = rucksack.chars().collect::<Vec<_>>();
-        let compartment_one = items[0..items.len() / 2].to_vec();
-        let compartment_two = items[items.len() / 2..].to_vec();
+    let rucksacks = get_rucksacks(data);
+    let priorities = rucksacks.iter().map(get_prioritized_value).sum::<i32>();
 
-        Rucksack {
-            compartment_one,
-            compartment_two,
-        }
-    });
-
-    let priorities = rucksacks.map(|rucksack| -> i32 {
-        rucksack
-            .shared_items()
-            .iter()
-            .map(|c| {
-                return match c {
-                    'a'..='z' => **c as i32 - 96,
-                    'A'..='Z' => **c as i32 - 38,
-                    _ => 0,
-                };
-            })
-            .max()
-            .unwrap_or_default()
-    });
-
-    priorities.sum::<i32>().to_string()
+    priorities.to_string()
 }
 
 fn solve_part_2(data: &str) -> String {
-    todo!();
+    let rucksacks = get_rucksacks(data);
+    let rucksack_groups = rucksacks.chunks(3);
+
+    let group_values = rucksack_groups.map(|group| {
+        let values = group.iter().flat_map(|rucksack| {
+            let mut priority_vaules = rucksack.to_priority_values();
+
+            priority_vaules.sort();
+            priority_vaules.dedup();
+
+            priority_vaules
+        });
+
+        let in_all_sacks = values
+            .clone()
+            .filter(move |v| values.clone().filter(|v2| v == v2).count() == 3);
+
+        in_all_sacks.max().unwrap_or_default()
+    });
+
+    let total = group_values.sum::<i32>();
+    total.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let data = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+        assert_eq!(solve_part_1(data), "157");
+    }
+
+    #[test]
+    fn test_part_2() {
+        let data = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+        assert_eq!(solve_part_2(data), "70");
+    }
 }

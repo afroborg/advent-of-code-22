173e5409a500ff08a9865056c9404c2d9340b72e
Axel Froborg <axel.froborg@gmail.com>
2022-12-09 17:41:45 +0100

    day 4 part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,22 +1,25 @@
 use std::{fs, ops::RangeInclusive};
 
-#[derive(Debug)]
-struct Elf {
-    range: RangeInclusive<i32>,
-}
+#[derive(Debug, Clone)]
+struct Elf(RangeInclusive<i32>);
 
 impl Elf {
     fn new(s: &str) -> Self {
         let start_stop: Vec<i32> = s.split('-').map(|s| s.parse::<i32>().unwrap()).collect();
 
-        Self {
-            range: start_stop[0]..=start_stop[1],
-        }
+        Self(start_stop[0]..=start_stop[1])
     }
 
-    fn fully_contains(&mut self, other: &mut Self) -> bool {
-        self.range.contains(&other.range.start()) && self.range.contains(&other.range.end())
-            || other.range.contains(&self.range.start()) && other.range.contains(&self.range.end())
+    fn fully_contains_another(&mut self, other: &mut Self) -> bool {
+        self.0.contains(&other.0.start()) && self.0.contains(&other.0.end())
+            || other.0.contains(&self.0.start()) && other.0.contains(&self.0.end())
+    }
+
+    fn partial_contains_another(&mut self, other: &mut Self) -> bool {
+        self.0.contains(&other.0.start())
+            || self.0.contains(&other.0.end())
+            || other.0.contains(&self.0.start())
+            || other.0.contains(&self.0.end())
     }
 }
 
@@ -34,7 +37,7 @@ fn solve_part_1(data: &str) -> String {
             let mut first = Elf::new(&parts[0]);
             let mut second = Elf::new(&parts[1]);
 
-            let intersects = first.fully_contains(&mut second);
+            let intersects = first.fully_contains_another(&mut second);
 
             intersects
         })
@@ -44,7 +47,20 @@ fn solve_part_1(data: &str) -> String {
 }
 
 fn solve_part_2(data: &str) -> String {
-    todo!();
+    let intersections = data
+        .lines()
+        .filter(|line| {
+            let parts = line.split(',').collect::<Vec<_>>();
+            let mut first = Elf::new(&parts[0]);
+            let mut second = Elf::new(&parts[1]);
+
+            let intersects = first.partial_contains_another(&mut second);
+
+            intersects
+        })
+        .count();
+
+    intersections.to_string()
 }
 #[cfg(test)]
 mod tests {
@@ -64,8 +80,13 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        let data = "";
+        let data = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
 
-        assert_eq!(solve_part_2(data), "");
+        assert_eq!(solve_part_2(data), "4");
     }
 }
